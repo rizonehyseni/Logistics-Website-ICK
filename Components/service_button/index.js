d0d232801adf8123c14b2bb5555e33219bc78d8f
lirkunushevcii <lirkunushevci@gmail.com>
2025-11-13 23:02:08 +0100

    switched service_name attribute from p to a

--- Components/service_button/index.js
+++ Components/service_button/index.js
@@ -9,9 +9,9 @@ class ServiceButtonComponent extends HTMLElement {
             <div class="service_button">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 4.5 21 12m0 0-7.5 7.5M21 12H3" /></svg>
-                <p>
+                <a href='#'>
                     ${service_name}
-                </p>
+                </a>
             </div>
         `
     }
